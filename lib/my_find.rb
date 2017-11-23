ef37f92fb55384f46ca5be938555e142a9863560
Liz Burton <liz.burton147@gmail.com>
2017-11-23 18:55:00 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -7,3 +7,12 @@ def my_find(collection)
     i += 1
   end
 end
+
+def my_collect(collection)
+  i = 0
+  return_array = []
+  while i < collection.length
+    return_array << collection[i] if yield(collection[i])
+    i += 1
+  end
+end
