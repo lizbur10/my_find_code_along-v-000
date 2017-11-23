142bf5bf76263e1f656dfc2803b92e6cbe20c4c2
Liz Burton <liz.burton147@gmail.com>
2017-11-23 18:43:35 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 def my_find(collection)
   i = 0
+  return_collection = [0]
   while i < collection.length
-    yield(collection[i])
+    return_collection << yield(collection[i])
     i += 1
   end
 end
